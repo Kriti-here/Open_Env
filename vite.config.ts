9dc6b9bf15cfaee822ca36ce5710f903122dbefb
Kriti <kritii29@users.noreply.huggingface.co>
2026-04-08 14:56:14 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: true,
     port: 7860,
     allowedHosts: ['kritii29-open-env.hf.space']
+  },
+  build: {
+    outDir: 'dist'
   }
 })
